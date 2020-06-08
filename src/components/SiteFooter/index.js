c9e77816d24bb8c33592929479eac887be0cedab
sarah semark <sarah@octopusthink.com>
2020-06-08 15:25:32 +0100

    fix: Attempt to add copyright to footer.
    This doesn't work, so I'll try a different approach.

--- src/components/SiteFooter/index.js
+++ src/components/SiteFooter/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const { site } = useStaticQuery(graphql`
+  query {
+    site {
+      siteMetadata {
+        copyright
+      }
+    }
+  }
+`);
 
 const SiteFooter = () => {
-  return <footer></footer>;
+  return <footer>{site.siteMetadata.copyright}</footer>;
 };
 
 export default SiteFooter;
