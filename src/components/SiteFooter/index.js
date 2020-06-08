4ea59af9265c5b4c03d417e50ccd3f275d8348c8
sarah semark <sarah@octopusthink.com>
2020-06-08 15:32:26 +0100

    feat: Show copyright line in footer.
    Fix #29.

--- src/components/SiteFooter/index.js
+++ src/components/SiteFooter/index.js
@@ -1,18 +1,27 @@
+import { css } from '@emotion/core';
+import { Paragraph } from '@octopusthink/nautilus';
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 
-const { site } = useStaticQuery(graphql`
-  query {
-    site {
-      siteMetadata {
-        copyright
-      }
-    }
-  }
-`);
+import config from 'data/SiteConfig';
 
 const SiteFooter = () => {
-  return <footer>{site.siteMetadata.copyright}</footer>;
+  return (
+    <footer
+      css={css`
+        border-top: 2px solid;
+        margin-top: 4.8rem;
+        margin: 0 auto;
+        max-width: ${config.contentWidth};
+        max-width: ${config.siteContentWidth};
+        padding: 2.4rem 0;
+        text-align: center;
+      `}
+    >
+      <Paragraph small light>
+        {config.copyright}
+      </Paragraph>
+    </footer>
+  );
 };
 
 export default SiteFooter;
